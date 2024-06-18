3c380d4c945ad02bb6cac5b9987dbc39cf6028e3
MAGd_ <lgls.magd@gmail.com>
2024-06-18 10:05:03 +0800

    contain main menu

--- app/src/main/java/com/example/ea4501/MainActivity.java
+++ app/src/main/java/com/example/ea4501/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.ea4501;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void showRanking(View view) {
+        Intent intent = new Intent(this, RankingActivity.class);
+        startActivity(intent);
+    }
+
+    public void showRecords(View view) {
+        Intent intent = new Intent(this, RecordsActivity.class);
+        startActivity(intent);
+    }
+
+    public void closeApp(View view) {
+        finish();
     }
 }
